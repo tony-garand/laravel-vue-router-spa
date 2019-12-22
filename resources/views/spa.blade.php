95ed75f3aff008a0aad847c7c29673c6b94ad837
tony-garand <anesthetics1@gmail.com>
2019-12-22 14:25:22 -0800

    removig unneeded view

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Vue SPA Demo</title>
+    <title>Vue Laravel SPA</title>
 </head>
 <body>
     <div id="app">
